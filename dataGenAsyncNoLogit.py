ad783ad906cf8d223e0929a8f9d3b554a0e7d988
P-H-B-D <98623538+P-H-B-D@users.noreply.github.com>
2023-08-09 23:50:44 -0400

    more data

--- dataGenAsyncNoLogit.py
+++ dataGenAsyncNoLogit.py
@@ -4,16 +4,23 @@ import openai
 import tiktoken
 import openai_async
 import matplotlib.pyplot as plt
+import numpy as np
+
 
 with open('secretkey.txt', 'r') as f:
     secret = f.readline()
 
-low=1
-high=10
+low=0
+high=6.999999
 samples=1000
 
+float_range = np.arange(low, high+0.1, 0.1) # float from 0.7 to 0.9
+
+
+
+
 enc = tiktoken.encoding_for_model("gpt-3.5-turbo")
-possible_nums=range(low,high+1) # int from 1 to 10
+# possible_nums=range(low,high+1) # int from 1 to 10
 
 
 async def generateRating(metrics): #because logit_bias doesn't work for floats or large numbers due to tokenization, we have to be a bit more careful with the response.
@@ -25,28 +32,30 @@ async def generateRating(metrics): #because logit_bias doesn't work for floats o
                 "model": "gpt-3.5-turbo",
                 "messages": [{"role": "user", "content": f"generate a random number from {str(low)} to {(high)}. Number only, no text."}],
                 "temperature": 0.7,
-                "max_tokens": 1,
+                "max_tokens": 5,
                 # "logit_bias": possible_tokens #won't work for floats or large numbers due to tokenization 
 
             },
         )
+        # print(f"generate a random number from {str(low)} to {(high)}. Number only, no text.")
         return completion
-
+ 
     try:
         completion=await genResponse()
 
         try:
-            res=(int(completion.json()['choices'][0]['message']['content']))
+            res=(float(completion.json()['choices'][0]['message']['content']))
             return res
 
         except:
             while True:
                 completion=await genResponse()
                 try:
-                    res=(int(completion.json()['choices'][0]['message']['content']))
+                    res=(float(completion.json()['choices'][0]['message']['content']))
                     return res
            
                 except:
+                    print(completion.json()['choices'][0]['message']['content'])
                     tries+=1
                     if tries>5:
                         res=None
@@ -75,35 +84,41 @@ async def run_concurrent_calls():
     # Run the coroutines concurrently
     results = await asyncio.gather(*tasks)
     print(results)
-
-
-    # # Print or process the results as needed
-    print(len(results))
-    results=[int(i) for i in results if i is not None]
-    print("mean: ", sum(results)/len(results), "versus expected: ", (high+low)/2)
-    # Plot histogram
-    # Compute weights for each data point such that the histogram sums up to 1
-    #save results to a csv
     with open(f'{str(low)}_{str(high)}_n{len(results)}_nologit.csv', 'w') as f:
         for i in results:
             f.write(str(i)+",\n")
-    data=results
+
+
+    # # # Print or process the results as needed
+    # # print(len(results))
+    # results=[float(i) for i in results if i is not None]
+    # print("mean: ", sum(results)/len(results), "versus expected: ", (high+low)/2)
+
+
+    # # # Plot histogram
+    # # # Compute weights for each data point such that the histogram sums up to 1
+    # # #save results to a csv
+    # # with open(f'{str(low)}_{str(high)}_n{len(results)}_nologit.csv', 'w') as f:
+    # #     for i in results:
+    # #         f.write(str(i)+",\n")
+    # data=results
     
-    weights = [1/len(data) for _ in data]
+    # weights = [1/len(data) for _ in data]
 
-    # Plot histogram with proportions
-    plt.hist(data, bins=range(low, high+2), edgecolor="k", align='left', weights=weights) #works well for ints, but not for floats
-    expected_proportion = 1/len(range(low, high+1))
-    plt.axhline(y=expected_proportion, color='r', linestyle='--', label="Expected Proportion")
+    # # Plot histogram with proportions
+    # plt.hist(data, bins=float_range, edgecolor="k", align='left', weights=weights) #works well for ints, but not for floats
+    
+    # expected_proportion = 1/(len(float_range))
+    # plt.axhline(y=expected_proportion, color='r', linestyle='--', label="Expected Proportion")
 
-    # Set the title and labels
-    plt.title(f'Integers from {low} to {high}, n= '+str(len(data)))
-    plt.xlabel('Value')
-    plt.ylabel('Proportion')
-    plt.xticks(list(range(low, high+1)))
+    # # Set the title and labels
+    # plt.title(f'Integers from {low} to {high}, n= '+str(len(data)))
+    # plt.xlabel('Value')
+    # plt.ylabel('Proportion')
+    # plt.xticks(list(float_range))
 
-    # Display the histogram
-    plt.show()
+    # # Display the histogram
+    # plt.show()
 
 
 if __name__ == "__main__":
