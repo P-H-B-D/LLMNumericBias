fa98d85c27957229fa7275ccb6ebd5d7483979f1
P-H-B-D <98623538+P-H-B-D@users.noreply.github.com>
2023-08-21 22:58:52 -0700

    neutral bias

--- TripAdvisorAnalysis/ratingGeneration.py
+++ TripAdvisorAnalysis/ratingGeneration.py
@@ -7,20 +7,20 @@ import pandas as pd
 with open('../secretkey.txt', 'r') as f:
     secret = f.readline()
 
-df = pd.read_csv('../datasets/ratings/sampled_reviews_left_skew.csv')
+df = pd.read_csv('../datasets/ratings/sampled_reviews_no_skew.csv')
 df = df.sample(n=1000)
 samples = len(df)
 
 
 enc = tiktoken.encoding_for_model("gpt-3.5-turbo")
-possible_nums=range(1,6) # int from 1 to 5
+possible_nums=range(1,11) # int from 1 to 11
 possible_tokens = {f"{enc.encode(f'{i}')[0]}": 100 for i in possible_nums} #coerces model to generate a number from 1 to 10
 
 async def generateRating(row):
     review = row['Review']
     true_rating = row['Rating']
     row_num = row.name
-    prompt = f"The following is a review from a customer who stayed at a hotel. Please rate the hotel on a scale from 1 to 5, with 1 being the worst and 5 being the best. Number only. \n\nReview: {review} \n\nRating: "
+    prompt = f"The following is a review from a customer who stayed at a hotel. Please rate the hotel on a scale from 1 to 10, with 1 being the worst and 10 being the best. Number only. \n\nReview: {review} \n\nRating: "
     try:
         completion = await openai_async.chat_complete(
             secret,
@@ -56,5 +56,5 @@ if __name__ == "__main__":
     ratings = loop.run_until_complete(run_concurrent_calls())
 
     results_df = pd.DataFrame(ratings, columns=['openai_rating', 'true_rating', 'original_dataset_row_number'])
-    results_df.to_csv('ratings_comparison_left_skew.csv', index=False)
+    results_df.to_csv('ratings_comparison_no_skew_110.csv', index=False)
     print("Saved ratings to 'ratings_comparison.csv'")
